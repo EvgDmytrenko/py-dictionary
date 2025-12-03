003a55490c4a33b9d19dd1c2de543556d50a898a
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2025-12-03 23:56:47 +0200

    add first functions

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
 class Dictionary:
-    pass
-    pass
+    def __init__(self) -> None:
+        self.length = 0
+        self.hash_table = [None] * 8
+
+    def __len__(self) -> int:
+        return self.length
+
+    def __set_item__(self, key, value) -> None:
+        index = hash(key)
+
+
