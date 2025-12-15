4f0f09673edef1ca403003849bc10178e0b491b7
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2025-12-15 23:05:28 +0200

    add three basic functions

--- app/main.py
+++ app/main.py
@@ -9,11 +9,12 @@ class Node:
 
 
 class Dictionary:
+
     def __init__(self) -> None:
         self.__capacity = 8
         self.__length = 0
         self.__hash_table = [None] * self.__capacity
-        self.__load_factor = 2/3
+        self.__load_factor = 2 / 3
 
     def __len__(self) -> int:
         return self.__length
@@ -26,9 +27,15 @@ class Dictionary:
             self.__length += 1
         else:
             if self.__hash_table[index].key != key:
-                index = Dictionary.find_bucket(self.__hash_table, index, self.__capacity)
-                self.__hash_table[index] = node
-                self.__length += 1
+                for i in range(1, self.__capacity):
+                    new_index = (index + i) % self.__capacity
+                    if self.__hash_table[new_index] is None:
+                        self.__length += 1
+                        break
+                    if self.__hash_table[new_index].key == key:
+                        break
+
+                self.__hash_table[new_index] = node
             else:
                 self.__hash_table[index] = node
 
@@ -52,45 +59,43 @@ class Dictionary:
             if node is not None:
                 new_index = node.hash % new_capacity
                 if new_hash_table[new_index] is not None:
-                    new_index = Dictionary.find_bucket(new_hash_table, new_index, new_capacity)
+                    new_index = Dictionary.find_bucket(
+                        new_hash_table, new_index, new_capacity)
                 new_hash_table[new_index] = node
 
         self.__hash_table = new_hash_table
         self.__capacity = new_capacity
 
-    def __getitem__(self, key) -> None:
+    def __getitem__(self, key: Any) -> Any | None:
+        index = self.__find_index_of_element(key)
+        return self.__hash_table[index].value
+
+    def __delitem__(self, key: Any) -> None:
+        index = self.__find_index_of_element(key)
+        self.__hash_table[index] = None
+        self.__length -= 1
+
+    def __find_index_of_element(self, key: Any) -> int:
         _hash = hash(key)
         index = _hash % self.__capacity
         if self.__hash_table[index] is None:
             raise KeyError(f"Key {key} not found!")
         if self.__hash_table[index].key != key:
-            raise KeyError(f"Key {key} not found!")
-
-        return self.__hash_table[index].value
+            for i in range(1, self.__capacity):
+                new_index = (index + i) % self.__capacity
+                if self.__hash_table[new_index] is None:
+                    raise KeyError(f"Key {key} not found!")
+                if self.__hash_table[new_index].key == key:
+                    break
+            return new_index
+        return index
 
     def clear(self) -> None:
         self.__hash_table = [None] * self.__capacity
         self.__length = 0
 
-    def __delitem__(self, key) -> None:
-        _hash = hash(key)
-        index = _hash % self.__capacity
-        if self.__hash_table[index] is None:
-            raise KeyError(f"Key {key} not found!")
-        if self.__hash_table[index].key != key:
-            raise KeyError(f"Key {key} not found!")
-
-        del self.__hash_table[index]
-        self.__length -= 1
-
-
-custom_dict = Dictionary()
-custom_dict[8] = "8"
-custom_dict[16] = "16"
-custom_dict[32] = "32"
-custom_dict["one"] = 1
-custom_dict["one"] = 11
-custom_dict["f"] = 60
-custom_dict["g"] = 70
-# value = custom_dict["a"]
-# value2 = custom_dict["b"]
+    def get(self, key: Any, default: None) -> Any | None:
+        try:
+            return self.__getitem__(key)
+        except KeyError:
+            return default
